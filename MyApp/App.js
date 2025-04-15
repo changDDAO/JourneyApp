4cea7ba73d7c891c68201deda81e8b1ceb672ee8
changDDAO <younch8342@naver.com>
2025-04-15 16:34:53 +0900

    App에서 Style적용 해보기

--- MyApp/App.js
+++ MyApp/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.bigBlue}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bigBlue:{
+    color:'blue',
+    fontWeight: 'bold',
+    fontSize : 30
+  },
+  red :{
+    color:'red'
+  }
 });
